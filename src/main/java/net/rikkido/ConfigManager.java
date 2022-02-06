f419c975343c4c67759de8e64a888893a35e1f09
maki nohira <masayoshi55@live.jp>
2022-02-06 12:52:12 +0900

    to set finish radius by config

--- src/main/java/net/rikkido/ConfigManager.java
+++ src/main/java/net/rikkido/ConfigManager.java
@@ -6,9 +6,10 @@ public class ConfigManager {
 
     private Zipline _plugin;
 
-    public Config<Double> Speed = new Config<Double>("speed", 1.0);
-
-    public Config<Double> MaxRadiusZipline = new Config<Double>("zipline_max_radius", -1.0);//negative value means infinity
+    public Config<Double> Speed = new Config<Double>("zipline.speed", 1.0);// block per tick
+    public Config<Double> MaxRadiusZipline = new Config<Double>("zipline.max_radius", -1.0);// negative value means
+                                                                                            // infinity
+    public Config<Double> ZipliningFinishRadius = new Config<Double>("ziplining.finizh_raduis", 0.5);
 
     public ConfigManager(Zipline plugin) {
         _plugin = plugin;
@@ -20,5 +21,6 @@ public class ConfigManager {
     private void loadConfig(FileConfiguration config) {
         Speed.value = config.getDouble(Speed.configName);
         MaxRadiusZipline.value = config.getDouble(MaxRadiusZipline.configName);
+        ZipliningFinishRadius.value = config.getDouble(ZipliningFinishRadius.configName);
     }
 }
